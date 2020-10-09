e0116f0a43dddba9aa3aae6b92bc45658085bbed
Kirillstrelbitskiy <strelOK042021@gmail.com>
2020-10-09 09:36:38 +0300

    Update

--- arduino_part/arduino_part.ino
+++ arduino_part/arduino_part.ino
@@ -3,7 +3,7 @@
 IRrecv irrecv(7);
 decode_results results;
 
-int number_of_devices = 2;
+int number_of_devices = 4;
 int devices_ports[] = {3, 2, 4, 5, 6};
 char devices_names[] = {'a', 'b', 'c', 'd', 'e'};
 
@@ -13,7 +13,7 @@ void setup() {
   
   for (int i = 0; i < number_of_devices; i++) {
     pinMode(devices_ports[i], OUTPUT);
-    digitalWrite(devices_ports[i], true);
+    digitalWrite(devices_ports[i], false);
   }
 }
 
@@ -43,16 +43,34 @@ void loop() {
         command = recieved_command[1];
 
     int device_id = find_device_id(device);
-
+    Serial.println(device_id);
     if(device_id != -1){
-      if (command == '1')
-        digitalWrite(devices_ports[device_id], true);
-      else if (command == '0')
-        digitalWrite(devices_ports[device_id], false);
-    
+      if(device_id == 2)
+        change_computer_state();
+      else if(device_id == 3)
+        power_on_computer();
+      else{
+        if (command == '1')
+          digitalWrite(devices_ports[device_id], true);
+        else if (command == '0')
+          digitalWrite(devices_ports[device_id], false);
+      }
+      
       Serial.println("Ready");
     }
   }
+}
+
+void change_computer_state(){
+  digitalWrite(devices_ports[2], HIGH);
+  delay(2000);
+  digitalWrite(devices_ports[2], LOW);
+}
+
+void power_on_computer(){
+  digitalWrite(devices_ports[3], HIGH);
+  delay(500);
+  digitalWrite(devices_ports[3], LOW);
 }
 
 int find_device_id(char device_name) {
